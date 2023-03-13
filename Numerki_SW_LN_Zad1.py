f74b509ee406a94191e52e76a565b3cdda3ff8da
Nowogórska Laura <242479@edu.p.lodz.pl>
2023-03-13 13:18:36 +0100

    [Added] fourth function and option for iteration
    Program pyta teraz użytkownika o dane

--- Numerki_SW_LN_Zad1.py
+++ Numerki_SW_LN_Zad1.py
@@ -12,20 +12,29 @@ def trygonometryczna(x):
     # f(x) = sin(x)
     return float(math.sin(x))
 
+def zlozenie(x):
+    # f(x) = 2x - 4 + arctan(x)
+    return 2*x - 4 + (math.atan(x))
+
 def bisekcja(fun, low, high, eps = None, iter = None):
-    fLow = fun(low) * 1.0
+    # fLow - dolna część przedziału
+    fLow = fun(low) * 1.0   
+    # fHigh - górna część przedziału
     fHigh = fun(high) * 1.0
     repetition = 0
 
+    # Sprawdza, czy rozwiązanie funkcji nie występuje na którymś z końców przedziału
     if (fun(low) == 0): 
         return low
     elif (fun(high) == 0): 
         return high
     
+    # Sprawdza, czy początek i koniec przedziału posiada różne znaki
     if (fLow * fHigh > 0):
         return "Error"
     else:
-        while(eps != None and abs(low-high) > eps):
+        # Wykonuje, dopóki wartość absolutna różnicy nie jest mniejsza od zadanego epsilona
+        while(eps != None and abs(low-high) > eps or iter != None):
 
             if (iter != None and repetition == iter):
                 break
@@ -40,8 +49,46 @@ def bisekcja(fun, low, high, eps = None, iter = None):
             else:
                 low = mid
 
-        return "Pierwiastek: " + str(mid) + "\n" + "Wartosc funkcji: " + str(fun(mid))
-    
-print(bisekcja(wielomian, -10.0, 10.0, 1e-14, None) + "\n")
-print(bisekcja(wykladnicza, -10.0, 10.0, 1e-14, None) + "\n") 
-print(bisekcja(trygonometryczna, 1.0, 10.0, 1e-14, None) + "\n") 
+        return "Pierwiastek: " + str(mid) + "\n" + "Liczba iteracji: " + str(repetition)
+
+# Podany epsilon 
+# print(bisekcja(wielomian, -10.0, 10.0, 1e-14, None) + "\n")
+# print(bisekcja(wykladnicza, -10.0, 10.0, 1e-14, None) + "\n") 
+# print(bisekcja(trygonometryczna, 1.0, 10.0, 1e-14, None) + "\n") 
+# print(bisekcja(zlozenie, 0.0, 4.0, 1e-14, None) + "\n") 
+
+# # Podana liczba iteracji
+# print(bisekcja(wielomian, -10.0, 10.0, None, 20) + "\n")
+# print(bisekcja(wykladnicza, -10.0, 10.0, None, 20) + "\n") 
+# print(bisekcja(trygonometryczna, 1.0, 10.0, None, 20) + "\n") 
+# print(bisekcja(zlozenie, 0.0, 4.0, None, 20) + "\n") 
+
+# print("Wybierz funkcję, której rozwiązanie chcesz znaleźć:\n1 - wielomian\n2 - wykładnicza\n3 - trygonometryczna\n4 - złożona")
+# fun = input()
+# print("Wpisz dolną granicę funkcji: ")
+# low = float(input())
+# print("Wpisz górną granicę funkcji: ")
+# high = float(input())
+# print("Określ kryterium zatrzymania algorytmu:\n1 - warunek dokładności\n2 - liczba iteracji")
+# kryterium = input()
+# if kryterium == "1":
+#     print("Podaj wartość epsilon(np. 1e-8): ")
+#     eps = float(input())
+#     iter = None
+# elif kryterium == "2":
+#     print("Podaj liczbę iteracji: ")
+#     iter = int(input())
+#     eps = None
+# else:
+#     print("Nie wybrano odpowiedniego kryterium")
+# match fun:
+#     case '1':
+#         print(bisekcja(wielomian, low, high, eps, iter))
+#     case '2':
+#          print(bisekcja(wykladnicza, low, high, eps, iter))
+#     case '3':
+#         print(bisekcja(trygonometryczna, low, high, eps, iter))
+#     case '4':
+#          print(bisekcja(zlozenie, low, high, eps, iter))
+
+
