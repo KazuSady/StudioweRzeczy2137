222759439996dcb85c6e3f34f0418f1a12bf2ab2
Nowogórska Laura <242479@edu.p.lodz.pl>
2023-03-15 16:47:50 +0100

    [Changed] Again returns and one function

--- Numerki_SW_LN_Zad1.py
+++ Numerki_SW_LN_Zad1.py
@@ -1,8 +1,8 @@
 import math
 
 def wielomian(x):
-    # f(x) = 2x3 - 6x2 + 2x - 1
-    return x*(x*(x-3) + 2)-6
+    # f(x) = 2x^3 - 6x^2 + 2x - 1
+    return 2*pow(x,3)-6*x*x+2*x-1
 
 def wykladnicza(x):
     # f(x) = ( (1/3)^x - 3)
@@ -42,9 +42,9 @@ def bisekcja(fun, low, high, eps, iter):
 
     # Sprawdza, czy rozwiązanie funkcji nie występuje na którymś z końców przedziału
     if (fun(low) == 0): 
-        return str(low)
+        return "Pierwiastek: " + str(low) + "\nLiczba iteracji: 1"
     elif (fun(high) == 0): 
-        return str(high)
+        return "Pierwiastek: " +  str(high) + "\nLiczba iteracji: 1"
     
     # Sprawdza, czy początek i koniec przedziału posiada różne znaki
     if (fLow * fHigh > 0):
@@ -81,58 +81,36 @@ def styczna(fun, Dfun, low, high, eps, iter):
     return "Pierwiastek: " + str(tmp) + "\nLiczba iteracji: " + str(repetition)
 
 
-# Podany epsilon 
-# print(bisekcja(wielomian, -1.0, 10.0, 1e-8, None) + "\n")
-print(bisekcja(wykladnicza, -1.0, 10.0, 1e-8, None) + "\n" ) 
-# print(bisekcja(trygonometryczna, -1.0, 6.0, 1e-8, None) + "\n") 
-# print(bisekcja(zlozenie, 0.0, 4.0, 1e-8, None) + "\n") 
-
-# print(styczna(wielomian, Dwielomian, -1.0, 10.0, 1e-8, None) + "\n")
-print(styczna(wykladnicza, Dwykladnicza, -1.0, 10.0, 1e-8, None) + "\n") 
-# print(styczna(trygonometryczna, Dtrygonometryczna, 0.0, 4.0, 1e-8, None) + "\n") 
-# print(styczna(zlozenie, Dzlozenie, 0.0, 4.0, 1e-8, None) + "\n") 
-
-# Podana liczba iteracji
-# print(bisekcja(wielomian, -10.0, 10.0, None, 20) + "\n")
-# print(bisekcja(wykladnicza, -10.0, 10.0, None, 20) + "\n") 
-# print(bisekcja(trygonometryczna, -10.0, 10.0, None, 20) + "\n") 
-# print(bisekcja(zlozenie, 0.0, 4.0, None, 20) + "\n") 
-
-# print(styczna(wielomian, Dwielomian,-10.0, 10.0, None, 20) + "\n")
-# print(styczna(wykladnicza, Dwykladnicza,-10.0, 10.0, None, 20) + "\n") 
-# print(styczna(trygonometryczna, Dtrygonometryczna, 1.0, 4.0, None, 20) + "\n") 
-# print(styczna(zlozenie, Dzlozenie, 0.0, 4.0, None, 20) + "\n") 
-
-# print("Wybierz funkcję, której rozwiązanie chcesz znaleźć:\n1 - wielomian\n2 - wykładnicza\n3 - trygonometryczna\n4 - złożona")
-# fun = input()
-# print("Wpisz dolną granicę funkcji: ")
-# low = float(input())
-# print("Wpisz górną granicę funkcji: ")
-# high = float(input())
-# print("Określ kryterium zatrzymania algorytmu:\n1 - warunek dokładności\n2 - liczba iteracji")
-# kryterium = input()
-# if kryterium == "1":
-#     print("Podaj wartość epsilon(np. 1e-8): ")
-#     eps = float(input())
-#     iter = None
-# elif kryterium == "2":
-#     print("Podaj liczbę iteracji: ")
-#     iter = int(input())
-#     eps = None
-# else:
-#     print("Nie wybrano odpowiedniego kryterium")
-# match fun:
-#     case '1':
-#         print(bisekcja(wielomian, low, high, eps, iter))
-#         print(styczna(wielomian, Dwielomian, low, high, eps, iter))
-#     case '2':
-#          print(bisekcja(wykladnicza, low, high, eps, iter))
-#          print(bisekcja(wykladnicza, Dwykladnicza, low, high, eps, iter))
-#     case '3':
-#         print(bisekcja(trygonometryczna, low, high, eps, iter))
-#         print(bisekcja(trygonometryczna, Dtrygonometryczna, low, high, eps, iter))
-#     case '4':
-#          print(bisekcja(zlozenie, low, high, eps, iter))
-#          print(bisekcja(zlozenie, Dzlozenie, low, high, eps, iter))
+print("Wybierz funkcję, której rozwiązanie chcesz znaleźć:\n1 - wielomian\n2 - wykładnicza\n3 - trygonometryczna\n4 - złożona")
+fun = input()
+print("Wpisz dolną granicę funkcji: ")
+low = float(input())
+print("Wpisz górną granicę funkcji: ")
+high = float(input())
+print("Określ kryterium zatrzymania algorytmu:\n1 - warunek dokładności\n2 - liczba iteracji")
+kryterium = input()
+if kryterium == "1":
+    print("Podaj wartość epsilon(np. 1e-8): ")
+    eps = float(input())
+    iter = None
+elif kryterium == "2":
+    print("Podaj liczbę iteracji: ")
+    iter = int(input())
+    eps = None
+else:
+    print("Nie wybrano odpowiedniego kryterium")
+match fun:
+    case '1':
+        print(bisekcja(wielomian, low, high, eps, iter))
+        print(styczna(wielomian, Dwielomian, low, high, eps, iter))
+    case '2':
+         print(bisekcja(wykladnicza, low, high, eps, iter))
+         print(styczna(wykladnicza, Dwykladnicza, low, high, eps, iter))
+    case '3':
+        print(bisekcja(trygonometryczna, low, high, eps, iter))
+        print(styczna(trygonometryczna, Dtrygonometryczna, low, high, eps, iter))
+    case '4':
+         print(bisekcja(zlozenie, low, high, eps, iter))
+         print(styczna(zlozenie, Dzlozenie, low, high, eps, iter))
 
 
