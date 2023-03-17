e0e10fa0f7d0d45f2b773c6413d47ab2989d5d28
242568 Szymon Wydmuch <242456@edu.p.lodz.pl>
2023-03-17 09:44:20 +0100

    Numerki wykresy fixed

--- Numerki/Numerki_SW_LN_Zad1.py
+++ Numerki/Numerki_SW_LN_Zad1.py
@@ -6,13 +6,13 @@ def wielomian(x):
     # f(x) = 2x^3 - 6x^2 + 2x - 1
     return 2*pow(x,3)-6*x*x+2*x-1
 
-def rysuj_funkcje(fun, low, high, root_x):
+def rysuj_funkcje(fun, low, high, root_x1, root_x2):
     x = np.linspace(low, high, 100)
     y = fun(x)
     plt.plot(x, y)
     plt.grid()
 
-    root_y = fun(root_x)
+    root_y = fun(root_x1)
 
     ax = plt.gca()
     ax.spines['top'].set_color('none')
@@ -22,8 +22,11 @@ def rysuj_funkcje(fun, low, high, root_x):
     ax.set_xlabel('x', size=14, labelpad=-24, x=1.03)
     ax.set_ylabel('y', size=14, labelpad=-21, y=1.02, rotation=0)
 
-    plt.axvline(x=root_x, color='r', linestyle='--')
-    plt.text(root_x, root_y, f'({root_x:.4f}, {root_y:.4f})', horizontalalignment='right' if root_x < (low + high) / 2 else 'left')
+    plt.axvline(x=root_x1, color='r', linestyle='--')
+    plt.text(root_x1, root_y, f'({root_x1:.4f}, {root_y:.4f})', horizontalalignment='right' if root_x1 < (low + high) / 2 else 'left')
+
+    plt.axvline(x=root_x2, color='g', linestyle=':')
+    plt.text(root_x2, root_y, f'({root_x2:.4f}, {root_y:.4f})', horizontalalignment='right' if root_x2 < (low + high) / 2 else 'left')
 
     plt.show()
     
@@ -66,9 +69,9 @@ def bisekcja(fun, low, high, eps, iter):
 
     # Sprawdza, czy rozwiązanie funkcji nie występuje na którymś z końców przedziału
     if (fun(low) == 0): 
-        return "Pierwiastek: " + str(low) + "\nLiczba iteracji: 1"
+        return "Pierwiastek: " + str(low) + "\nLiczba iteracji: 1" , low
     elif (fun(high) == 0): 
-        return "Pierwiastek: " +  str(high) + "\nLiczba iteracji: 1"
+        return "Pierwiastek: " +  str(high) + "\nLiczba iteracji: 1", high
     
     # Sprawdza, czy początek i koniec przedziału posiada różne znaki
     if (fLow * fHigh > 0):
@@ -89,7 +92,7 @@ def bisekcja(fun, low, high, eps, iter):
                 high = mid
             else:
                 low = mid
-        return "Pierwiastek: " + str(mid) + "\nLiczba iteracji: " + str(repetition)
+        return "Pierwiastek: " + str(mid) + "\nLiczba iteracji: " + str(repetition), mid
 
 def styczna(fun, Dfun, low, high, eps, iter):
     repetition = 0
@@ -123,20 +126,26 @@ else:
     print("Nie wybrano odpowiedniego kryterium")
 match fun:
     case '1':
-        print(bisekcja(wielomian, low, high, eps, iter))
+        print(bisekcja(wielomian, low, high, eps, iter)[0])
         print(styczna(wielomian, Dwielomian, low, high, eps, iter)[0])
-        rysuj_funkcje(wielomian,low,high,styczna(wielomian, Dwielomian, low, high, eps, iter)[1])
+        x1 = styczna(wielomian, Dwielomian, low, high, eps, iter)[1]
+        x2 = bisekcja(wielomian, low, high, eps, iter)[1]
+        rysuj_funkcje(wielomian,low,high,x1,x2)
     case '2':
-        print(bisekcja(wykladnicza, low, high, eps, iter))
+        print(bisekcja(wykladnicza, low, high, eps, iter)[0])
         print(styczna(wykladnicza, Dwykladnicza, low, high, eps, iter)[0])
-        rysuj_funkcje(wykladnicza,low,high,styczna(wykladnicza, Dwykladnicza, low, high, eps, iter)[1])
+        x1 = styczna(wykladnicza, Dwykladnicza, low, high, eps, iter)[1]
+        x2 = bisekcja(wykladnicza, low, high, eps, iter)[1]
+        rysuj_funkcje(wykladnicza, low,high,x1,x2)
     case '3':
-        print(bisekcja(trygonometryczna, low, high, eps, iter))
+        print(bisekcja(trygonometryczna, low, high, eps, iter)[0])
         print(styczna(trygonometryczna, Dtrygonometryczna, low, high, eps, iter)[0])
-        rysuj_funkcje(trygonometryczna,low,high,styczna(trygonometryczna, Dtrygonometryczna, low, high, eps, iter)[1])
+        x1 = styczna(trygonometryczna, Dtrygonometryczna, low, high, eps, iter)[1]
+        x2 = bisekcja(trygonometryczna, low, high, eps, iter)[1]
+        rysuj_funkcje(trygonometryczna, low,high,x1,x2)
     case '4':
-        print(bisekcja(zlozenie, low, high, eps, iter))
+        print(bisekcja(zlozenie, low, high, eps, iter)[0])
         print(styczna(zlozenie, Dzlozenie, low, high, eps, iter)[0])
-        rysuj_funkcje(zlozenie,low,high,styczna(zlozenie, Dzlozenie, low, high, eps, iter)[1])
-
-
+        x1 = styczna(zlozenie, Dzlozenie, low, high, eps, iter)[1]
+        x2 = bisekcja(zlozenie, low, high, eps, iter)[1]
+        rysuj_funkcje(zlozenie,low,high,x1,x2)
---
+++ Telekomuna/Zad1/Zadanie1/Zadanie1/Main.cs
@@ -0,0 +1,10 @@
+﻿using Zadanie1;
+
+Kod kod = new Kod();
+
+String filePath = "C:\\Users\\Husaiin\\Desktop\\Studia\\SEM 4\\StudioweRzeczy2137\\Telekomuna\\Zad1\\Zadanie1\\kekw.txt";
+
+FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+
+Console.WriteLine(kod.kodowanie(fs));
+
