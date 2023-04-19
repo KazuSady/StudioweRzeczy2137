6d53fc728d9d6337585d0abbcf1eab415d425ae2
SzczupakKrolemPontaru <szymonwydmuch1@gmail.com>
2023-04-19 17:51:12 +0200

    [Finished] Final DESX [Refactor] Changed DESX to encrypt files

--- Krypto/DESX/DESX/DESX.cs
+++ Krypto/DESX/DESX/DESX.cs
@@ -4,114 +4,111 @@ using System.Linq;
 using System.Text;
 using System.ComponentModel.Design;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Messaging;
 
 namespace DESX
 {
     internal class DESX
     {
         private Permutation permutation = new Permutation();
-        private string result;
+        private byte [] result;
         List<byte[]> messageBlocks = new List<byte[]>();
         List<byte[]> keys = new List<byte[]>();
 
 
         // Jezeli decrypt = true, odwracamy kolność użycia kluczy co pozwala na wykonanie całego proceszu szyfrowania tym samym kodem tylko od tyłu
         // Tutaj odwracamy klucze Internal i External wykorzystane kolejno przed i po zaszyfrowaniu wiadomosci DES'em, w DES'ie odwracamy wszystki 16 48-bitowych podkluczy 
-        public string encrypt(char[] message, string keyInternal, string keyDes, string keyExternal, bool decrypt)
+        public byte[] encrypt(byte[] message, string keyInternal, string keyDes, string keyExternal, bool decrypt)
         {
+            // Obliczenie długości wypełnienia do długości podzielnej przez 8 
+            int paddedLength = ((message.Length + 7) / 8) * 8;
+            int toFill = paddedLength - message.Length;
+            byte[] paddedMessage = new byte[paddedLength];
+            result = new byte[paddedLength];
+
             keys.Add(permutation.charToByteArray(keyInternal.ToCharArray(), 64));
             keys.Add(permutation.charToByteArray(keyDes.ToCharArray(), 64));
             keys.Add(permutation.charToByteArray(keyExternal.ToCharArray(), 64));
 
+            Array.Copy(message, paddedMessage, message.Length);
+            // Wypełnienie tablicy do długości podzielnej przez 8 liczbą która reprezentuje różnice między aktualną długością i najbliższą większą liczbą podzielną przez 8 
+            // Dla przykładu
+            // message.Length = 25
+            // paddedMessage.Length = 32
+            // toFill = 32 - 25 = 7
+            // Indexy od 26 do 31 będą miał wartość 7 
+            for (int i = message.Length; i < paddedMessage.Length; i++)
+            {
+                paddedMessage[i] = (byte)toFill;
+            }       
+            
+            // Podzielenie całej wiadomości na 8 elementowe tablice
+            splitToGroups(paddedMessage);
+            
+            // Odwrócenie całej operacji szyfrowania w przypadku deszyfrowania
             if (decrypt)
             {
                 keys.Reverse();
             }
 
-            string tmpResult = "";
-
-            char[] messagePadding; 
-            if (!decrypt)
-            {
-                messagePadding = addPadding(message);
-            }
-            else
-            {
-                messagePadding = message;
-            }
-
-            divideBlocks(messagePadding);
-
             for (int i = 0; i < messageBlocks.Count; i++)
             {
                 DES des = new DES();
-                byte[] blockXOR1 = permutation.xor(messageBlocks.ElementAt(i), keys.ElementAt(0));
+                // Zamiana 8 elementowej tablicy 8 bajtowych liczb na ich reprezentacje binarne
+                byte[] block = _8ByteTabTo64BitsTab(messageBlocks.ElementAt(i));
+                byte[] blockXOR1 = permutation.xor(block, keys.ElementAt(0));
                 byte[] blockDES = des.enryptBlock(blockXOR1, keys.ElementAt(1), decrypt);
                 byte[] blockXOR2 = permutation.xor(blockDES, keys.ElementAt(2));
-                result += permutation.showASCII(blockXOR2);
-                int kekw = result.Length;
+                Array.Copy(_64BitTabTo8ByteTab(blockXOR2), 0, result, i * 8, 8);
             }
 
+            // Obcięcie niepotrzebnego wypełnienia zastosowanego w szyfrowaniu
             if (decrypt)
             {
-                int toTrim = (int)result[result.Length - 1];
-                int trim = message.Length - toTrim;
-                if (trim < 0)
-                {
-                    trim = message.Length;
-                }
-                for (int i = 0; i < trim; i++)
-                {
-                    tmpResult += result[i];
-                }
-                return tmpResult;
+                int trim = (result[result.Length - 1]);
+                return result.Take(result.Length - trim).ToArray();
             }
-            else
-            {
-                return result;
-            }
+
+            return result;
         }
 
-        private void divideBlocks(char [] message)
+        private void splitToGroups(byte[] message)
         {
-            char[] tmpBlock = new char[8];
-            byte[] block = new byte[64];
-            int index = 0;
-            for (int i = 0; i < (message.Length); i++)
+            for (int i = 0; i < message.Length; i += 8)
             {
-                tmpBlock[index] = message[i];
-                index++;
-                if (index == 8)
+                ulong block = 0;
+                for (int j = 0; j < 8 && i + j < message.Length; j++)
                 {
-                    block = permutation.charToByteArray(tmpBlock, 64);
-                    index = 0;
-                    messageBlocks.Add(block);
+                    block |= (ulong)message[i + j] << (8 * j);
                 }
+
+                byte[] blockBytes = BitConverter.GetBytes(block);
+                messageBlocks.Add(blockBytes);
             }
         }
-        private char [] addPadding(char[] message)
+        private byte[] _8ByteTabTo64BitsTab(byte[] tab)
         {
+            byte[] result = new byte[64];
 
-            int trim = 8 - (message.Length % 8);
-            char[] tmpMessage = new char[message.Length + trim];
-            if (trim == 8)
+            for (int i = 0; i < tab.Length; i++)
             {
-                trim = 0;
+                string binary = Convert.ToString(tab[i], 2).PadLeft(8, '0');
+                byte [] binaryTab = binary.Select(c => (byte)(c - '0')).ToArray();
+                Array.Copy(binaryTab, 0, result, i*8, 8);
             }
-            for (int i = 0; i < message.Length + trim; i++)
-            {
-                if (i < message.Length)
-                {
-                    tmpMessage[i] = message[i];
-                }
-                else
-                {
-                    tmpMessage[i] = Convert.ToChar(trim);
-                }
-            }
-            return tmpMessage;
+            return result;
         }
+        private byte[] _64BitTabTo8ByteTab(byte[] tab)
+        { 
+            byte[] result = new byte[8];
 
+            for (int i = 0; i < 8; i++)
+            {
+                result[i] = tab.Skip(i * 8).Take(8).Aggregate((byte)0, (a, b) => (byte)((a << 1) + b));
+            }
+            return result;
+        }
         public List<String> generateKeys()
         {
             List<string> keysRandom = new List<string>();
@@ -131,5 +128,6 @@ namespace DESX
 
             return keysRandom;
         }
+
     }
 }
--- Krypto/DESX/DESX/Program.cs
+++ Krypto/DESX/DESX/Program.cs
@@ -16,21 +16,24 @@ namespace DESX
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new UI());
-
+            /*
             DESX desx = new DESX();
             DESX desx2 = new DESX();
 
-            string message = "The key size is thereby increased to 56 + (2 × 64) = 184 bits.However, the effective key size (security) is only increased to 56+64−1−lb(M) = 119 − lb(M) = ~119 bits, where M is the number of chosen plaintext/ciphertext pairs the adversary can obtain, and lb denotes the binary logarithm. Moreover, key size drops to 88 bits given 232.5 known plaintext and using advanced slide attack.DES-X also increases the strength of DES against differential cryptanalysis and linear cryptanalysis, although the improvement is much smaller than in the case of brute force attacks. It is estimated that differential cryptanalysis would require 261 chosen plaintexts (vs. 247 for DES), while linear cryptanalysis would require 260 known plaintexts (vs. 243 for DES or 261 for DES with independent subkeys.[1]) Note that with 264 plaintexts (known or chosen being the same in this case), DES (or indeed any other block cipher with a 64 bit block size) is totally broken as the whole cipher's codebook becomes available.Although the differential and linear attacks, currently best attack on DES-X is a known-plaintext slide attack discovered by Biryukov-Wagner [2] which has complexity of 232.5 known plaintexts and 287.5 time of analysis. Moreover the attack is easily converted into a ciphertext-only attack with the same data complexity and 295 offline time complexity.";
+            byte[] message = Encoding.GetEncoding("ISO-8859-2").GetBytes("The key size is thereby increased to 56 + (2 × 64) = 184 bits.");
             string keyInternal = "87654321";
             string key = "12345678";
             string keyExternal = "43218765";
 
-            string messageEncrypted = desx.encrypt(message.ToCharArray(), keyInternal, key, keyExternal, false);
-            string messageDecrypted = desx2.encrypt(messageEncrypted.ToCharArray(), keyInternal, key, keyExternal, true);
+            string messageEncrypted = desx.encrypt(message, keyInternal, key, keyExternal, false);
+            Console.WriteLine(messageEncrypted.Length);
+            byte[] messageEncryptedByte = Encoding.GetEncoding("ISO-8859-2").GetBytes(messageEncrypted);
+
+            string messageDecrypted = desx2.encrypt(messageEncryptedByte, keyInternal, key, keyExternal, true);
 
             Console.WriteLine(messageEncrypted);
             Console.WriteLine(messageDecrypted);
-
+            */
         }
     }
 
