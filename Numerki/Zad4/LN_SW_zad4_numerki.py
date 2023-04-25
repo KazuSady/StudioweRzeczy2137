aa54901940a5464cf513fcd0326e968a6116a857
Nowogórska Laura <242479@edu.p.lodz.pl>
2023-04-25 18:32:22 +0200

    [Changed] Zad4
    Need to work on Newton as it doesn't return the same value as Gauss

--- Numerki/Zad4/LN_SW_zad4_numerki.py
+++ Numerki/Zad4/LN_SW_zad4_numerki.py
@@ -10,18 +10,10 @@ def f2(x):
 def f3(x):
     return 4 * np.log(x + 3)
 #------------------------------------------------------
-# Horner
-def Horner(stopien, a, x):
-    wynik = a[stopien]
-    for i in range(stopien - 1, -1, -1):
-        wynik = wynik * x + a[i]
-    return wynik
-
-#------------------------------------------------------
 # Simpson part 1
 def Simpson1(a, b, funkcja):
     h = (b - a) / 2
-    result = h/3 * (e**(- a)* funkcja(a) + 4 * e**(-(a+b)/2) * funkcja((a+b)/2) + funkcja(b) * e**(- b))
+    result = h/3 * (e**(- a*a)* funkcja(a) + 4 * e**(-((a+b)/2)*((a+b)/2)) * funkcja((a+b)/2) + funkcja(b) * e**(- b*b))
     return result
 
 #------------------------------------------------------
@@ -49,10 +41,10 @@ def Simpson2(a, b, funkcja, eps):
 #------------------------------------------------------
 # Newton-Cotes
 def newton_cotes(funkcja, eps):
-    a = 0
     delta = 1
     result = 0
     iter = 1
+    a=-10
     while True:
         resS = Simpson2(a, a + delta, funkcja, eps)
         result += resS
@@ -63,20 +55,75 @@ def newton_cotes(funkcja, eps):
     print(a, iter)
     return result
 #------------------------------------------------------
+# Współczynniki dla kwadratury Gaussa
+def wspolczynnik(n):
+    s = (n,2)
+    tabwspol = np.zeros(s)
+    match n:
+        case 2:
+            tabwspol[0][0] = -0.707107
+            tabwspol[0][1] = 0.886227
+            tabwspol[1][0] = 0.707107
+            tabwspol[1][1] = 0.886227
+        case 3:
+            tabwspol[0][0] = -1.224745
+            tabwspol[0][1] = 0.295409
+            tabwspol[1][0] = 0.000000
+            tabwspol[1][1] = 1.181636
+            tabwspol[2][0] = 1.224745
+            tabwspol[2][1] = 0.295409
+        case 4:
+            tabwspol[0][0] = -1.650680
+            tabwspol[0][1] = 0.081313
+            tabwspol[1][0] = -0.534648
+            tabwspol[1][1] = 0.804914
+            tabwspol[2][0] = 0.534648
+            tabwspol[2][1] = 0.804914
+            tabwspol[3][0] = 1.650680
+            tabwspol[3][1] = 0.081313
+        case 5:
+            tabwspol[0][0] = -2.020183
+            tabwspol[0][1] = 0.019953
+            tabwspol[1][0] = -0.958572
+            tabwspol[1][1] = 0.393619
+            tabwspol[2][0] = 0.000000
+            tabwspol[2][1] = 0.945309
+            tabwspol[3][0] = 0.958572
+            tabwspol[3][1] = 0.393619
+            tabwspol[4][0] = 2.020183
+            tabwspol[4][1] = 0.019953
+    return tabwspol
+#------------------------------------------------------
+# Gauss-Hermite
+def hermite(wezly, funkcja):
+    result = 0
+    tmp = 0
+    wspol = wspolczynnik(wezly)
+    for i in range(wezly):
+        if wspol[i][1] != 0:
+            tmp = wspol[i][1] * funkcja(wspol[i][0])
+            result += tmp
+    return result
+#------------------------------------------------------
 # Menu
-print("Wybierz funkcję:\n1 - |x-5|\n2 - cos(2x)\n3 - 4log(x+3)")
+print("Wybierz funkcję:\n1 - |x-5|\n2 - 2^x\n3 - 4log(x+3)")
 choice = input()
+print("Podaj ilość węzłów dla kwadratury Gaussa: 2/3/4/5")
+wezly = int(input())
 print("Podaj dokładność:")
 eps = float(input())
 
 match choice:
     case '1':
         przyb_cal = newton_cotes(f1, eps)
+        gausherm = hermite(wezly, f1)
     case '2':
        przyb_cal = newton_cotes(f2, eps) 
+       gausherm = hermite(wezly, f2)
     case '3':
         przyb_cal = newton_cotes(f3, eps)
-print(przyb_cal)
+        gausherm = hermite(wezly, f3)
+print(przyb_cal, gausherm)
  
 #------------------------------------------------------   
       
